eebde9562eeb56ba38daed8bca79d601031dd93a
heriwei <617443938@qq.com>
2019-04-08 17:56:42 +0800

    2019.04.08学习ajax

---
+++ H5-JD/js/index.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    pageScroll();
+}
+
+function pageScroll() {
+    window.onscroll = function () {
+        var scrollY = window.sc;
+        console.log("滚动距离:" + scrollX);
+    }
+}
